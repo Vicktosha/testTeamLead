d8f55690555f874770cc84653e3d74d3c7adabc4
Виктор <viktor@MacBook-Pro-2.local>
2023-01-25 23:30:30 +0300

    adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -7,10 +7,10 @@ $('.review__carousel').slick({
     slidesToScroll: 1,
     prevArrow: false,
     nextArrow: false,
-    dots: true,
+    // dots: true,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
@@ -18,14 +18,14 @@ $('.review__carousel').slick({
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 992,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
